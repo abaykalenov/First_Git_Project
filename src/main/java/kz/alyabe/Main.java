c656e2756001b5816cbc66b48fe21800272021a4
abaykalenov <atas0231@gmail.com>
2025-11-28 00:19:19 +0500

    Add second sout method

--- src/main/java/kz/alyabe/Main.java
+++ src/main/java/kz/alyabe/Main.java
@@ -3,5 +3,6 @@ package kz.alyabe;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Abay's first commit using intellij idea");
     }
 }
